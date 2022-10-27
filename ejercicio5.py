e843c344ab4c9e7beaca575fbd72cdb96eb80b8c
valuvilla <valuvp@hotmail.com>
2022-10-27 10:58:59 +0200

    5

---
+++ ejercicio5.py
@@ -0,0 +1,26 @@
+import sys
+
+def num():
+    while True:
+        numeros=input("Valor: ")
+        try:
+            numeros=int(numeros)
+        except:
+            print("valor no aceptado", file=sys.stderr)
+        else:
+            break
+            sys.exit()
+    return str(numeros)
+
+def descomponer():
+    numeros=num()
+    numero=numeros[::-1]
+    ceros_izquierda=0
+    while True:
+        for i in str(numero):
+            ceros_izquierda+=1
+            print((i.ljust(int(ceros_izquierda),'0')).zfill(len(numero)), "\n")
+        if ceros_izquierda >= len(numero):
+            break
+
+(descomponer())
