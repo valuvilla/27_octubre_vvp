25cd9815cdc5846e221bbcb6e38d4c8b21a30ce5
valuvilla <valuvp@hotmail.com>
2022-10-27 11:18:28 +0200

    readMe

---
+++ recorrido.py
@@ -0,0 +1,21 @@
+"""
+Crea un Script llamado recorrido.py que realice las siguientes funciones:
+
+Recorre el listado del ejemplo y realiza las siguientes operaciones: [18, 50, 210, 80, 145, 333, 70, 30]
+
+Imprimr el número en caso de que sea múltiplo de 10 y menor que 200
+Parar el programa si llega a un número mayor que 300
+"""
+
+def recorrer():
+    lista=[18, 50, 210, 80, 145, 333, 70, 30]
+    nueva_lista=[]
+    while True:
+        for i in lista:
+            if i % 10 == 0 and i<200:
+                nueva_lista.append(i)
+                return nueva_lista
+            if i>300:
+                break
+
+print(recorrer())
