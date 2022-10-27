003f91541a0c8f28df5a702b6020d324e335b5fe
valuvilla <valuvp@hotmail.com>
2022-10-27 11:11:29 +0200

    6

---
+++ ejericico7.py
@@ -0,0 +1,7 @@
+"""
+Crea un Script llamado recorrido.py que realice las siguientes funciones:
+
+Recorre el listado del ejemplo y realiza las siguientes operaciones: [18, 50, 210, 80, 145, 333, 70, 30]
+
+Imprimr el número en caso de que sea múltiplo de 10 y menor que 200
+"""
